f7552579c9b50c98ca7b76e4e3c845a77ebc89d3
Ryan Rodwell <ryanrodwell@gmail.com>
2017-07-10 14:51:38 -0400

    testing video

--- client/templates/PortfolioTemplate.xml.js
+++ client/templates/PortfolioTemplate.xml.js
@@ -31,7 +31,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
                   <lockup videoURL="http://www.rwdevcon.com/videos/Ray-Wenderlich-Teamwork.mp4">
                     <img src="${this.BASEURL}images/advanced-trivia.png" width="500" height="308" />
                   </lockup>
-                  <lockup videoURL="http://www.rwdevcon.com/videos/Ryan-Nystrom-Contributing.mp4">
+                  <lockup videoURL="http://ryanrodwell.com/videos/burger_demo">
                     <img src="${this.BASEURL}images/eat-da-burger.png" width="500" height="308" />
                   </lockup>								
                   <lockup videoURL="http://www.rwdevcon.com/videos/Vicki-Wenderlich-Identity.mp4">
