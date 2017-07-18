b60bf1043ab6e4b3129addb6eb49c19d418f13a2
Ryan Rodwell <ryanrodwell@gmail.com>
2017-07-18 00:24:19 -0400

    editing layout, adding more projects

--- client/templates/PortfolioTemplate.xml.js
+++ client/templates/PortfolioTemplate.xml.js
@@ -7,15 +7,18 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
       <list> 
         <section> 
           <listItemLockup> 
-            <title>Projects</title>
+            <title>Group Projects</title>
             <decorationLabel>3</decorationLabel>
             <relatedContent> 
               <grid>
-                <section> 
-                     <lockup videoURL="http://www.rwdevcon.com/videos/Matthijs-Hollemans-Math-Isnt-Scary.mp4">
+                <section>
+                    <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/signaturefd.mp4">
+                        <img src="${this.BASEURL}images/signaturefd.png" width="500" height="308" />
+                    </lockup>  
+                     <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/illegalformation.mp4">
                         <img src="${this.BASEURL}images/illegal-formation.png" width="500" height="308" />
-                  </lockup>	
-                    <lockup videoURL="http://www.rwdevcon.com/videos/Chris-Wagner-Craftsmanship.mp4">
+                    </lockup>	
+                    <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/stylecast.mp4">
                         <img src="${this.BASEURL}images/stylecast.png" width="500" height="308" />
                      </lockup>	
                 </section>
@@ -23,29 +26,58 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
             </relatedContent>
           </listItemLockup>
           <listItemLockup> 
-            <title>Homeworks</title>
-            <decorationLabel>10</decorationLabel>
+            <title>Bootcamp Assignments</title>
+            <decorationLabel>9</decorationLabel>
             <relatedContent> 
               <grid>
                 <section> 
+                  <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/cheeriomongo.mp4">
+                    <img src="${this.BASEURL}images/cheeriomongo.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/bamazon.mp4">
+                    <img src="${this.BASEURL}images/bamazon.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/nytreact.mp4">
+                    <img src="${this.BASEURL}images/nytreact.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/burger.mp4">
+                    <img src="${this.BASEURL}images/eat-da-burger.png" width="500" height="308" />
+                  </lockup> 
+                  <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/liri.mp4">
+                    <img src="${this.BASEURL}images/node-liri.png" width="500" height="308" />
+                      </lockup>
+                  <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/flashcards.mp4">
+                    <img src="${this.BASEURL}images/node-flashcards.png" width="500" height="308" />
+                  </lockup>
                   <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/advancedtrivia.mp4">
                     <img src="${this.BASEURL}images/advanced-trivia.png" width="500" height="308" />
                   </lockup>
-                  <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/advancedtrivia.mp4">
-                    <img src="${this.BASEURL}images/eat-da-burger.png" width="500" height="308" />
-                  </lockup>								
-                  <lockup videoURL="http://www.rwdevcon.com/videos/Vicki-Wenderlich-Identity.mp4">
-                    <img src="${this.BASEURL}images/node-flashcards.png" width="500" height="308" />
-                  </lockup>									
-                      <lockup videoURL="http://www.rwdevcon.com/videos/Alexis-Gallagher-Identity.mp4">
-                    <img src="${this.BASEURL}images/node-liri.png" width="500" height="308" />
-                      </lockup>									
+                  <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/trainschedule.mp4">
+                    <img src="${this.BASEURL}images/train-schedule.png" width="500" height="308" />
+                  </lockup>
                   <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/psychicgame.mp4">
                     <img src="${this.BASEURL}images/psychic-game.png" width="500" height="308" />
                   </lockup>									
-                  <lockup videoURL="http://www.rwdevcon.com/videos/Cesare-Rocchi-Cognition.mp4">
-                    <img src="${this.BASEURL}images/train-schedule.png" width="500" height="308" />
-                  </lockup>									
+                  									
+                </section>
+              </grid>
+            </relatedContent>
+          </listItemLockup>
+          <listItemLockup> 
+            <title>Personal Projects</title>
+            <decorationLabel>3</decorationLabel>
+            <relatedContent> 
+              <grid>
+                <section>
+                    <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/gtdb.mp4>
+                        <img src="${this.BASEURL}images/gtdb.png" width="500" height="308" />
+                    </lockup>  
+                     <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/peachpit.mp4>
+                        <img src="${this.BASEURL}images/peachpit.png" width="500" height="308" />
+                    </lockup>
+                    <lockup videoURL="http://rr-portfolio-videos.s3.amazonaws.com/prp.mp4>
+                        <img src="${this.BASEURL}images/prp.png" width="500" height="308" />
+                    </lockup>
                 </section>
               </grid>
             </relatedContent>
